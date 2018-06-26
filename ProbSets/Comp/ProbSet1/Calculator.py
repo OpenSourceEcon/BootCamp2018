454d522ae1b2b4baaee433d6148dd5a7a25e1aac
Suleyman Gozen <sgozen@uchicago.edu>
2018-06-26 13:08:28 -0500

    update

---
+++ ProbSets/Comp/ProbSet1/Calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 25 19:47:41 2018
+
+@author: suleymangozen
+"""
+import math
+
+def sum (a, b):
+    return a + b
+
+def sqrt(a):
+    return math.sqrt(a)
+
+def prod(a,b):
+    return a*b
